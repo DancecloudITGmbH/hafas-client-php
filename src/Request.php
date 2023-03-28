6d2e172c0e2dfceb4bd2e4c92c3375dd0c95ae53
Christoph Berghuber <christoph.berghuber@nimbuscloud.at>
2023-03-28 15:28:32 +0200

    bugfix: reiseauskunft does not support user-agent=hafas-client-php anymore

--- src/Request.php
+++ src/Request.php
@@ -17,7 +17,7 @@ abstract class Request {
      * @return stdClass
      * @throws GuzzleException
      */
-    public static function request(array $data, string $userAgent = 'hafas-client-php'): stdClass {
+    public static function request(array $data, string $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0'): stdClass {
         $client = new Client();
 
         $dummy              = self::getDummyRequestBody();
